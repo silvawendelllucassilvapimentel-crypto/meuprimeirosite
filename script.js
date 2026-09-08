4528b1fcfe30edba7895a9ed451e23d273e34013
silvawendelllucassilvapimentel-crypto <silvawendelllucassilvapimentel@gmail.com>
2026-09-08 13:32:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-let botaoNao = document.getElementById("nao");
+const sim = document.getElementById("sim");
+const nao = document.getElementById("nao");
 
-botaoNao.addEventListener("mouseover", function() {
+// Botão NÃO foge quando passa o mouse
+nao.addEventListener("mouseover", function () {
+    const largura = window.innerWidth - nao.offsetWidth;
+    const altura = window.innerHeight - nao.offsetHeight;
 
-    let x = Math.random() * 500;
-    let y = Math.random() * 300;
+    const x = Math.random() * largura;
+    const y = Math.random() * altura;
 
-    botaoNao.style.position = "absolute";
-    botaoNao.style.left = x + "px";
-    botaoNao.style.top = y + "px";
+    nao.style.position = "fixed";
+    nao.style.left = x + "px";
+    nao.style.top = y + "px";
+});
 
+// Quando clicar em SIM
+sim.addEventListener("click", function () {
+
+    nao.style.display = "none";
+    sim.style.display = "none";
+
+    document.querySelector("h1").innerHTML = "EU SABIA! ❤️";
+
+    // Cria os corações
+    for (let i = 0; i < 30; i++) {
+        const coracao = document.createElement("div");
+
+        coracao.innerHTML = "❤️";
+        coracao.classList.add("coracao");
+
+        coracao.style.left = Math.random() * 100 + "vw";
+        coracao.style.animationDuration =
+            (2 + Math.random() * 3) + "s";
+
+        coracao.style.animationDelay =
+            Math.random() * 2 + "s";
+
+        document.body.appendChild(coracao);
+
+        setTimeout(() => {
+            coracao.remove();
+        }, 6000);
+    }
 });
