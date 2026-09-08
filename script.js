aa5f26b08b369515b778670bac1188fe08de0b45
silvawendelllucassilvapimentel-crypto <silvawendelllucassilvapimentel@gmail.com>
2026-09-08 15:13:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,12 @@
-const sim = document.getElementById("sim");
-const nao = document.getElementById("nao");
+function mostrarMensagem() {
 
-// Botão NÃO foge quando passa o mouse
-nao.addEventListener("mouseover", function () {
-    const largura = window.innerWidth - nao.offsetWidth;
-    const altura = window.innerHeight - nao.offsetHeight;
+    alert("Bem-vindo ao Mercadão Resende! 🛒");
 
-    const x = Math.random() * largura;
-    const y = Math.random() * altura;
+}
 
-    nao.style.position = "fixed";
-    nao.style.left = x + "px";
-    nao.style.top = y + "px";
-});
+function mostrarContato() {
 
-// Quando clicar em SIM
-sim.addEventListener("click", function () {
+    document.getElementById("resultado").innerHTML =
+        "📞 Entre em contato com o Mercadão Resende!";
 
-    nao.style.display = "none";
-    sim.style.display = "none";
-
-    document.querySelector("h1").innerHTML = "EU SABIA! ❤️";
-
-    // Cria os corações
-    for (let i = 0; i < 30; i++) {
-        const coracao = document.createElement("div");
-
-        coracao.innerHTML = "❤️";
-        coracao.classList.add("coracao");
-
-        coracao.style.left = Math.random() * 100 + "vw";
-        coracao.style.animationDuration =
-            (2 + Math.random() * 3) + "s";
-
-        coracao.style.animationDelay =
-            Math.random() * 2 + "s";
-
-        document.body.appendChild(coracao);
-
-        setTimeout(() => {
-            coracao.remove();
-        }, 6000);
-    }
-});
+}
